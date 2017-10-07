1b1c9e80f99b2e40ef0bd9ac999c1ad05eee2c36
bulgakov <mail@dbulgakov.com>
2017-10-07 17:07:12 +0300

    updated pom file -> changed java ver, updated artifact id added initial socket listening logic in chat client

--- src/main/java/client/ChatClient.java
+++ src/main/java/client/ChatClient.java
@@ -1,4 +1,30 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ChatClient {
+    public static void main(String[] args) {
+        try (
+                Socket connection = new Socket("localhost", 9999);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                new BufferedInputStream(
+                                        connection.getInputStream())));
+
+                BufferedWriter out = new BufferedWriter(
+                        new OutputStreamWriter(
+                                new BufferedOutputStream(
+                                        connection.getOutputStream())))
+        ) {
+
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
