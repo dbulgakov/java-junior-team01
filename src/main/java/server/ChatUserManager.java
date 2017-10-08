5d072d0822f93f50f003616adba27a2f67f8f4bf
GuntikAS <losyonochek@yandex.ru>
2017-10-08 14:18:36 +0300

    rename users

--- src/main/java/server/ChatUserManager.java
+++ src/main/java/server/ChatUserManager.java
@@ -9,15 +9,18 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ChatUserManager {
-    private List<ChatUser> clients;
+    private List <ChatUser> clients;
+    
     private ReadWriteLock clientsLock;
     private History history;
-
+    int userCounter = 0;
+    
+    
     public ChatUserManager() {
-        this.clients = new ArrayList<>();
+        this.clients = new ArrayList <>();
         this.clientsLock = new ReentrantReadWriteLock();
     }
-
+    
     public void send(String messageToSend) throws IOException {
         clientsLock.readLock().lock();
         for (ChatUser client : clients) {
@@ -25,24 +28,45 @@ public class ChatUserManager {
         }
         clientsLock.readLock().unlock();
     }
-
+    
     public void addUser(ChatUser chatUser) {
         clientsLock.writeLock().lock();
+        try {
+            chatUser.rename("Anonymous" + userCounter);
+            userCounter++;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         clients.add(chatUser);
         clientsLock.writeLock().unlock();
     }
-
+    
     public void removeUser(ChatUser chatUser) {
         clientsLock.writeLock().lock();
         clients.remove(chatUser);
         clientsLock.writeLock().unlock();
     }
-
+    
     public History getHistory() {
         return history;
     }
-
+    
     public void setHistory(History history) {
         this.history = history;
+    }
+    
+    public boolean rename(String name, ChatUser chatUser) {
+        for (ChatUser client : clients) {
+            if ( client.getName().equals(name )) {
+                return false;
+            }
+        }
+        try {
+            chatUser.rename(name);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
