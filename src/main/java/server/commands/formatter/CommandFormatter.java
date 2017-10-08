8563d7c4066ed632eec1ce5fda439b11ba19b7eb
bulgakov <mail@dbulgakov.com>
2017-10-08 15:15:15 +0300

    minor fix3

--- src/main/java/server/commands/formatter/CommandFormatter.java
+++ src/main/java/server/commands/formatter/CommandFormatter.java
@@ -3,10 +3,14 @@ package server.commands.formatter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class CommandFormatter {
-	public static String  format(String name, LocalDateTime dateTime, String text){
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-		String formattedDateTime = formatter.format(dateTime);
-		return name + " " + formattedDateTime + " " + text;
-	}
+public final class CommandFormatter {
+    private CommandFormatter() {
+    }
+
+
+    public static String format(String name, LocalDateTime dateTime, String text) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDateTime = formatter.format(dateTime);
+        return name + " " + formattedDateTime + " " + text;
+    }
 }
